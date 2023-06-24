6fd770a34cee96964fd34e477dc94385a180a939
Rishi562 <rishiraj0911@gmail.com>
2023-06-24 10:53:40 +0530

    pushing emails

---
+++ 14. Emails/Email_Part1.py
@@ -0,0 +1,21 @@
+#it uses SMTP protocol
+
+import smtplib
+
+conn = smtplib.SMTP('smtp.gmail.com' , 587) #define the email server with port number
+
+type (conn)  #now we have connection object
+
+conn.ehlo() #call this to start the connection #ptint to see
+
+conn.starttls() #starts TLS
+conn.login('username', 'password')
+
+conn.sendmail('from','to','subject \n emailsub')
+
+conn.quit() #disconnect from smtp server
+
+#use app specific password provided by google
+
+
+
